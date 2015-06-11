e07d60ac25a62bb1e591014b42a995fcae1cdab1
Daniel Mikusa <dmikusa@pivotal.io>
2015-06-11 10:06:39 -0400

    Initial Commit

---
+++ Comic Character Carousel/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Comic Character Carousel
+//
+//  Created by Daniel Mikusa on 6/11/15.
+//  Copyright (c) 2015 Daniel Mikusa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
